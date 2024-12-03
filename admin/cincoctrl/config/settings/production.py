109779901c82c686c71d6e8a47f42bbc2dc68e84
amy wieliczka <amywieliczka@gmail.com>
2024-12-03 13:53:48 -0800

    Remove default_acl from s3 storage backend

--- admin/cincoctrl/config/settings/production.py
+++ admin/cincoctrl/config/settings/production.py
@@ -100,7 +100,7 @@ STORAGES = {
         "BACKEND": "storages.backends.s3.S3Storage",
         "OPTIONS": {
             "location": "static",
-            "default_acl": "public-read",
+            # "default_acl": "public-read",
         },
     },
 }
