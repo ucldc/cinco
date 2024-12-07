71baaa9b87294192ff630e238ad7b175dbcfcbef
amy wieliczka <amywieliczka@gmail.com>
2024-12-06 18:20:42 -0800

    Add wildcard * to ALLOWED_HOSTS

--- admin/cincoctrl/config/settings/production.py
+++ admin/cincoctrl/config/settings/production.py
@@ -21,6 +21,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
     "oac5.cdlib.org",
+    "*",
     "cinco-admin-alb-1944578478.us-west-2.elb.amazonaws.com",
     "52.34.189.245",  # public IP of the ALB
 ]
