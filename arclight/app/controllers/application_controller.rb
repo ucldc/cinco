28803ca71081282b4856c3ee1028ec22dd4a32d1
Esther Verreau <esther.verreau@ucop.edu>
2024-12-11 12:16:27 -0600

    add arclight project

---
+++ arclight/app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  # Adds a few additional behaviors into the application controller
+  include Blacklight::Controller
+  include Blacklight::LocalePicker::Concern
+  layout :determine_layout if respond_to? :layout
+
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+end
