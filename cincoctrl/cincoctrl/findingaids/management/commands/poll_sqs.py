f27956855f5ce9479e6d24fcca28c39d739796e5
amy wieliczka <amywieliczka@gmail.com>
2025-01-13 11:25:16 -0800

    Make a management command out of poll_sqs

--- cincoctrl/cincoctrl/findingaids/management/commands/poll_sqs.py
+++ cincoctrl/cincoctrl/findingaids/management/commands/poll_sqs.py
@@ -3,74 +3,82 @@ import os
 
 import boto3
 from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
 
 from cincoctrl.findingaids.models import SupplementaryFile
 
 s3_prefix = "textract-output"
 sqs_url = os.environ.get("SQS_URL")
 
-# SQS message structure:
-#   MessageId: str, ReceiptHandle: str, MD5OfBody: str
-#   Body: dict (SNS message)
 
-# SNS message structure:
-#   Type: str, MessageId: str, TopicArn: str, Timestamp: str,
-#   SignatureVersion: str, Signature: str, SigningCertURL: str,
-#   UnsubscribeURL: str
-#   Message: dict (textract message)
+class Command(BaseCommand):
+    help = "Poll the SQS queue for any messages."
 
+    def handle(self):
+        sqs = boto3.client("sqs")
+        response = sqs.receive_message(
+            QueueUrl=sqs_url,
+            MaxNumberOfMessages=10,
+            WaitTimeSeconds=20,  # Enable long polling
+        )
+        sqs_messages = response.get("Messages", [])
+        # SQS message structure: MessageId, ReceiptHandle, MD5OfBody,
+        #   Body (dict, SNS message)
 
-def get_queued_messages():
-    """
-    Poll the SQS queue for any messages.
-    """
-    sqs = boto3.client("sqs")
-    response = sqs.receive_message(
-        QueueUrl=sqs_url,
-        MaxNumberOfMessages=10,
-        WaitTimeSeconds=20,  # Enable long polling
-    )
-    sqs_messages = response.get("Messages", [])
-    for sqs_message in sqs_messages:
-        sns_message = json.loads(sqs_message.pop("Body"))
+        for sqs_message in sqs_messages:
+            sns_message = json.loads(sqs_message.pop("Body"))
+            # SNS message structure: Type, MessageId, TopicArn, Timestamp,
+            #   SignatureVersion, Signature, SigningCertURL, UnsubscribeURL,
+            #   Message (dict, textract message)
 
-        # if sns_message['TopicArn'] is AmazonTextract topic
-        textract_message = json.loads(sns_message.pop("Message"))
-        read_textract_message(textract_message)
-        # endif
+            # if sns_message['TopicArn'] is AmazonTextract topic
+            self.stdout.write(f"SNS topic is: {sns_message['TopicArn']}")
 
-        sqs.delete_message(QueueUrl=sqs_url, ReceiptHandle=sqs_message["ReceiptHandle"])
+            textract_message = json.loads(sns_message.pop("Message"))
+            self.read_textract_message(textract_message)
+            # endif
 
+            self.stdout.write(
+                f"Successfully read message {sqs_message['MessageId']} from SQS queue.",
+            )
 
-def read_textract_message(textract_message: dict) -> None:
-    """
-    Read the textract message off the SQS queue, find the SupplementaryFile
-    with the same pdf_file location, & update the status and textract_output
-    attributes of the corresponding SupplementaryFile object.
+            sqs.delete_message(
+                QueueUrl=sqs_url,
+                ReceiptHandle=sqs_message["ReceiptHandle"],
+            )
 
-    textract_message structure:
-        JobId: str, Status: str, API: str, JobTag: str, Timestamp: int,
-        DocumentLocation: dict[S3ObjectName: str, S3Bucket: str]
-    """
+    def read_textract_message(self, textract_message: dict) -> None:
+        """
+        Read the textract message off the SQS queue, find the SupplementaryFile
+        with the same pdf_file location, & update the status and textract_output
+        attributes of the corresponding SupplementaryFile object.
 
-    s3_obj = textract_message.get("DocumentLocation")
-    pdf_file = f"s3://{s3_obj['S3Bucket']}/{s3_obj['S3ObjectName']}"
-    supplementary_files = SupplementaryFile.objects.filter(pdf_file=pdf_file)
+        textract_message structure:
+            JobId: str, Status: str, API: str, JobTag: str, Timestamp: int,
+            DocumentLocation: dict[S3ObjectName: str, S3Bucket: str]
+        """
 
-    if len(supplementary_files) == 1:
-        supplementary_file = supplementary_files[0]
-    else:
-        error = f"Supplementary file not found for {pdf_file}"
-        raise ValueError(error)
+        s3_obj = textract_message.get("DocumentLocation")
+        pdf_file = f"s3://{s3_obj['S3Bucket']}/{s3_obj['S3ObjectName']}"
+        supplementary_files = SupplementaryFile.objects.filter(pdf_file=pdf_file)
 
-    status = textract_message.get("Status")
-    job_id = textract_message.get("JobId")
-    textract_output = f"s3://{settings.AWS_STORAGE_BUCKET_NAME}/{s3_prefix}/{job_id}"
+        if len(supplementary_files) == 1:
+            supplementary_file = supplementary_files[0]
+        else:
+            error = f"Supplementary file not found for {pdf_file}"
+            raise CommandError(error)
 
-    supplementary_file.status = status
-    supplementary_file.textract_output = textract_output
-    supplementary_file.save()
+        status = textract_message.get("Status")
+        job_id = textract_message.get("JobId")
+        textract_output = (
+            f"s3://{settings.AWS_STORAGE_BUCKET_NAME}/{s3_prefix}/{job_id}"
+        )
 
-    # find ead with specified supplemental file
-    # if all supplemental files for ead have status == SUCCEEDED,
-    # re-index ead?
+        supplementary_file.textract_status = status
+        supplementary_file.textract_output = textract_output
+        supplementary_file.save()
+
+        # find ead with specified supplemental file
+        # if all supplemental files for ead have status == SUCCEEDED,
+        # re-index ead?
