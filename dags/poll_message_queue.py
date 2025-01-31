4863cd3a3ca4456f333ad9c41ee19c85d659a46d
amy wieliczka <amywieliczka@gmail.com>
2025-01-31 15:55:55 -0800

    Add a poll message queue dag, make CincoCtrlOperator a bit more generic

---
+++ dags/poll_message_queue.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from airflow.decorators import dag
+
+from cinco.cincoctrl_operator import CincoCtrlOperator
+
+
+@dag(
+    dag_id="poll_message_queue",
+    schedule="@hourly",
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    tags=["cinco"],
+    # on_failure_callback=notify_dag_failure,
+    # on_success_callback=notify_dag_success,
+)
+def poll_message_queue():
+    poll_queue = CincoCtrlOperator(
+        task_id="poll_queue",
+        manage_cmd="poll_sqs",
+        # on_failure_callback=notify_failure,
+        # on_success_callback=notify_success
+    )
+
+
+poll_message_queue = poll_message_queue()
