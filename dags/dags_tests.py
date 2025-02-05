2f0f03895c4ad091c52541791bdb5f096a15a64d
amy wieliczka <amywieliczka@gmail.com>
2025-02-05 13:46:08 -0800

    DAGS: add dags test github workflow

---
+++ dags/dags_tests.py
@@ -0,0 +1,17 @@
+import sys
+
+from unittest import TestCase
+
+from airflow.models import DagBag
+
+sys.path.append(".")
+
+DAGS_FOLDER = "."
+
+
+class CincoDagsTest(TestCase):
+    def dag_bag(self):
+        return DagBag(dag_folder=DAGS_FOLDER, include_examples=False)
+
+    def test_no_import_errors(self):
+        assert not self.dag_bag().import_errors
