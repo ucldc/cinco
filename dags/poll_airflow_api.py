2b7db7aca05e001173a54ca42e981d86d31a38db
amy wieliczka <amywieliczka@gmail.com>
2025-03-04 14:52:39 -0800

    DAGS: Create a minutely polling of the airflow API

---
+++ dags/poll_airflow_api.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from airflow.decorators import dag
+
+from cinco.cincoctrl_operator import CincoCtrlOperator
+
+
+@dag(
+    dag_id="poll_airflow_api",
+    schedule="@minutely",
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    tags=["cinco"],
+    # on_failure_callback=notify_dag_failure,
+    # on_success_callback=notify_dag_success,
+)
+def cinco_poll_airflow_api():
+    poll_airflow = CincoCtrlOperator(  # noqa: F841
+        task_id="poll_airflow",
+        manage_cmd="poll_airflow",
+        # on_failure_callback=notify_failure,
+        # on_success_callback=notify_success
+    )
+
+
+cinco_poll_airflow_api = cinco_poll_airflow_api()
