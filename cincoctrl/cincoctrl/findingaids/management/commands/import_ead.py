32a4f712376e21401fe649b1007f59f29e1dc03a
Esther Verreau <esther.verreau@ucop.edu>
2025-03-05 15:46:23 -0600

    rearrange code, fix minor bugs

--- cincoctrl/cincoctrl/findingaids/management/commands/import_ead.py
+++ cincoctrl/cincoctrl/findingaids/management/commands/import_ead.py
@@ -26,6 +26,43 @@ class Command(BaseCommand):
             type=str,
         )
 
+    def validate_ead(self, filename, text):
+        parser = EADParser()
+        parser.parse_string(text)
+        parser.validate_dtd()
+        parser.validate_required_fields()
+        parser.validate_component_titles()
+        parser.validate_dates()
+        for e in parser.errors:
+            self.stdout.write(f"{filename}\t{e}\tERROR")
+        for w in parser.warnings:
+            self.stdout.write(f"{filename}\t{w}\tWARNING")
+        return parser
+
+    def process_supp_files(self, parser, doc_url, finding_aid):
+        # get and upload any supp files
+        urls = {}
+        order = 0
+        for a in parser.parse_otherfindaids():
+            r = requests.get(
+                (doc_url + a["href"]),
+                allow_redirects=True,
+                timeout=30,
+            )
+            sfilename = a["href"].split("/")[-1]
+            pdf_file = SimpleUploadedFile(sfilename, r.content)
+            s = SupplementaryFile.objects.create(
+                finding_aid=finding_aid,
+                title=a["text"],
+                pdf_file=pdf_file,
+                order=order
+            )
+            urls[a["href"]] = s.pdf_file.url
+            order += 1
+        # update links in original EAD
+        if len(urls) > 0:
+            parser.update_otherfindaids(urls)
+
     def handle(self, *args, **options):
         url = options.get("url")
         filename = url.split("/")[-1]
@@ -35,19 +72,11 @@ class Command(BaseCommand):
         r.raise_for_status()
 
         try:
-            parser = EADParser()
-            parser.parse_string(r.content)
-            parser.validate_dtd()
-            parser.validate_required_fields()
-            parser.validate_component_titles()
-            parser.validate_dates()
-            for e in parser.errors:
-                self.stdout.write(f"{filename}\t{e}\tERROR")
-            for w in parser.warnings:
-                self.stdout.write(f"{filename}\t{w}\tWARNING")
+            parser = self.validate_ead(filename, r.content)
             if len(parser.errors) == 0:
                 ark, parent_ark = parser.parse_arks()
                 repo = Repository.objects.get(ark=parent_ark)
+                parser.set_ark_dir(ark)
 
                 # create the finding aid without the file at first
                 f, _ = FindingAid.objects.get_or_create(
@@ -56,27 +85,10 @@ class Command(BaseCommand):
                     record_type="ead"
                 )
 
-                # get and upload any supp files
-                urls = {}
-                for a in parser.parse_otherfindaids():
-                    r = requests.get(
-                        (doc_url + a["href"]),
-                        allow_redirects=True,
-                        timeout=30,
-                    )
-                    sfilename = a["href"].split("/")[-1]
-                    pdf_file = SimpleUploadedFile(sfilename, r.content)
-                    s = SupplementaryFile.objects.create(
-                        finding_aid=f,
-                        title=a["text"],
-                        pdf_file=pdf_file,
-                    )
-                    urls[a["href"]] = s.pdf_file.url
-                # update links in original EAD
-                parser.update_otherfindaids(urls)
+                self.process_supp_files(parser, doc_url, f)
 
                 # add the new ead file
-                ead_file = SimpleUploadedFile(filename, parser.to_string())
+                ead_file = SimpleUploadedFile(filename, parser.to_string().encode(encoding="utf-8"))
                 f.ead_file = ead_file
                 f.save()
         except EADParserError as e:
