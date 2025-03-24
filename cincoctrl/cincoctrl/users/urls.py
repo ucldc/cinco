6a74136b6a2f037e864e984133a2083ef56b72a4
Esther Verreau <esther.verreau@ucop.edu>
2025-03-24 16:09:18 -0500

    fix change password form

--- cincoctrl/cincoctrl/users/urls.py
+++ cincoctrl/cincoctrl/users/urls.py
@@ -7,6 +7,6 @@ from .views import user_detail_view
 app_name = "users"
 urlpatterns = [
     path("<int:pk>/", view=user_detail_view, name="detail"),
-    path("<int:pk>/changepassword/", view=user_change_password, name="change_password"),
+    path("changepassword/", view=user_change_password, name="change_password"),
     path("repositories/", view=repository_list_view, name="list_repositories"),
 ]
