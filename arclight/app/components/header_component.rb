077f906a4c8e8f7a6127067b3505f0e97d7b04fe
Esther Verreau <esther.verreau@ucop.edu>
2025-04-14 12:06:13 -0500

    use our own custom header

---
+++ arclight/app/components/header_component.rb
@@ -0,0 +1,5 @@
+
+# Extend ArcLight's HeaderComponent
+# https://github.com/projectblacklight/arclight/blob/main/app/components/arclight/header_component.rb
+class HeaderComponent < Arclight::HeaderComponent
+end
