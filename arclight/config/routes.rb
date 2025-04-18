de4431b15a80d226de5549f492701bea5e11131b
Chad Nelson <chadbnelson@gmail.com>
2025-04-17 17:00:57 -0700

    Apply rubocop fixes

--- arclight/config/routes.rb
+++ arclight/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  mount Blacklight::Engine => '/'
-  mount Arclight::Engine => '/'
+  mount Blacklight::Engine => "/"
+  mount Arclight::Engine => "/"
 
   root to: "arclight/repositories#index"
   concern :searchable, Blacklight::Routes::Searchable.new
 
-  resource :catalog, only: [], as: 'catalog', path: '/catalog', controller: 'catalog' do
+  resource :catalog, only: [], as: "catalog", path: "/catalog", controller: "catalog" do
     concerns :searchable
   end
   devise_for :users
@@ -13,16 +13,16 @@ Rails.application.routes.draw do
   concern :exportable, Blacklight::Routes::Exportable.new
   concern :hierarchy, Arclight::Routes::Hierarchy.new
 
-  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+  resources :solr_documents, only: [ :show ], path: "/catalog", controller: "catalog" do
   concerns :hierarchy
     concerns :exportable
   end
 
-  resources :bookmarks, only: [:index, :update, :create, :destroy] do
+  resources :bookmarks, only: [ :index, :update, :create, :destroy ] do
     concerns :exportable
 
     collection do
-      delete 'clear'
+      delete "clear"
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- arclight/lib/oac/normalized_component_title.rb
+++ arclight/lib/oac/normalized_component_title.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'arclight/exceptions'
+require "arclight/exceptions"
 
 module Oac
   ##
@@ -12,7 +12,7 @@ module Oac
     # @param [String] `container_label` from `container`
     # @param [String] `unitid` from the `unitid`
     def initialize(title, date = nil, container_label = nil, unitid = nil)
-      @title = title.gsub(/\s*,\s*$/, '').strip if title.present?
+      @title = title.gsub(/\s*,\s*$/, "").strip if title.present?
       @date = date.strip if date.present?
       @container_label = container_label if container_label.present?
       @unitid = unitid if unitid.present?
@@ -28,7 +28,7 @@ module Oac
     attr_reader :title, :date, :container_label, :unitid, :default
 
     def normalize
-      result = [title, date].compact.join(', ')
+      result = [ title, date ].compact.join(", ")
       result = container_label if result.blank?
       result = unitid if result.blank?
 
