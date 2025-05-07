c483ef1d2ac98325562002c0a2655b9634207eba
Esther Verreau <esther.verreau@ucop.edu>
2025-05-07 13:20:30 -0500

    add a to z list for institutions, other display tweaks

--- arclight/app/controllers/arclight/repositories_controller.rb
+++ arclight/app/controllers/arclight/repositories_controller.rb
@@ -4,6 +4,7 @@ module Arclight
     class RepositoriesController < ApplicationController
         def index
             @repositories = Arclight::Repository.all
+            @repository_list = @repositories.group_by { |r| r.name[0] }
             load_collection_counts
         end
 
