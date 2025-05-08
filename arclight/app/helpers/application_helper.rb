60dd65d2c15befca84e2ff0cdb873a1941ddc19f
Esther Verreau <esther.verreau@ucop.edu>
2025-05-08 14:08:21 -0500

    pull out city from repo location

--- arclight/app/helpers/application_helper.rb
+++ arclight/app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
   def additional_locale_routing_scopes
     [ blacklight, arclight_engine ]
   end
+
+  def repository_location_to_city(location_str)
+    location_html = Nokogiri::HTML.fragment(location_str)
+    city_content = location_html.at('div[class="al-repository-street-address-city_state_zip_country"]').text
+    re = /(([A-Z][a-z]+\s?)+),\s([A-Z]{2})/
+    m = re.match(city_content)
+    if m
+      m[0]
+    else
+      location_html
+    end
+  end
 end
