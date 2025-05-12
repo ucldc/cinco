8afe0d302f9daf88a7e43a32f8828cd997ccdcdf
Barbara Hui <barbara.hui@ucop.edu>
2025-05-12 15:35:45 -0700

    Parameterize solr leader url

--- solr-replication-config.py
+++ solr-replication-config.py
@@ -18,37 +18,35 @@ if solr_config[-1].strip() == "":
 if solr_config[-1].strip() != "</config>":
     raise ValueError("Last line of solrconfig.xml is not '</config>'")
 
-leader_config = [
-    "  <!-- leader configuration for index replication -->\n"
-    '  <requestHandler name="/replication" class="solr.ReplicationHandler">\n',
-    '    <lst name="leader">\n',
-    '      <str name="replicateAfter">optimize</str>\n',
-    '      <str name="backupAfter">optimize</str>\n',
-    '      <str name="confFiles">_rest_managed.json,elevate.xml,mapping-ISOLatin1Accent.txt,protwords.txt,schema.xml,scripts.conf,spellings.txt,stopwords_en.txt,stopwords.txt,synonyms.txt</str>\n',
-    "    </lst>\n",
-    '    <int name="maxNumberOfBackups">2</int>\n',
-    '    <str name="commitReserveDuration">00:00:10</str>\n',
-    '    <lst name="invariants">\n',
-    '      <str name="maxWriteMBPerSec">16</str>\n',
-    "    </lst>\n",
-    "  </requestHandler>\n\n",
-]
-
-remote_host = "solr_leader"
-follower_config = [
-    "  <!-- follower configuration for index replication -->\n"
-    '  <requestHandler name="/replication" class="solr.ReplicationHandler">\n',
-    '    <lst name="follower">\n',
-    f'      <str name="leaderUrl">http://{remote_host}:8983/solr/arclight</str>\n',
-    '      <str name="pollInterval">00:00:20</str>\n',
-    "    </lst>\n",
-    "  </requestHandler>\n\n",
-]
-
 # insert leader/follower config
 if os.environ.get("REPLICATION_ROLE") == "leader":
+    leader_config = [
+        "  <!-- leader configuration for index replication -->\n"
+        '  <requestHandler name="/replication" class="solr.ReplicationHandler">\n',
+        '    <lst name="leader">\n',
+        '      <str name="replicateAfter">optimize</str>\n',
+        '      <str name="backupAfter">optimize</str>\n',
+        '      <str name="confFiles">_rest_managed.json,elevate.xml,mapping-ISOLatin1Accent.txt,protwords.txt,schema.xml,scripts.conf,spellings.txt,stopwords_en.txt,stopwords.txt,synonyms.txt</str>\n',
+        "    </lst>\n",
+        '    <int name="maxNumberOfBackups">2</int>\n',
+        '    <str name="commitReserveDuration">00:00:10</str>\n',
+        '    <lst name="invariants">\n',
+        '      <str name="maxWriteMBPerSec">16</str>\n',
+        "    </lst>\n",
+        "  </requestHandler>\n\n",
+    ]
     solr_config[-1:-1] = leader_config
 elif os.environ.get("REPLICATION_ROLE") == "follower":
+    solr_leader_url = os.environ["SOLR_LEADER_URL"]
+    follower_config = [
+        "  <!-- follower configuration for index replication -->\n"
+        '  <requestHandler name="/replication" class="solr.ReplicationHandler">\n',
+        '    <lst name="follower">\n',
+        f'      <str name="leaderUrl">{solr_leader_url}</str>\n',
+        '      <str name="pollInterval">00:00:20</str>\n',
+        "    </lst>\n",
+        "  </requestHandler>\n\n",
+    ]
     solr_config[-1:-1] = follower_config
 
 with open(solrconfig_path, "w") as f:
