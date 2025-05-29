7557ae50c1696a891821b128539f3544c7fec3ab
Barbara Hui <barbara.hui@ucop.edu>
2025-05-29 10:15:49 -0700

    Configure solr for index replication on docker startup (#366)
    Configure solr for index replication on docker startup. Modify ECS config to have only one solr task up at one time.

---
+++ cinco-docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Run script to configure as leader/follower for solr index replication
+if [ "$REPLICATION_ROLE" != "" ]; then
+    python3 /solr-replication-config.py
+    if [[ "$?" != 0 ]]; then
+        echo "Error running solr-replication-config.py"
+        exit 1
+    else
+        echo "Configured solr for replication role: $REPLICATION_ROLE"
+    fi
+else
+    echo "REPLICATION_ROLE env var not set; skipping configuration for solr index replication."
+fi
+
+# Run helper script to initialize an empty solr
+init-var-solr
+
+# create and start solr core
+exec solr-precreate arclight /opt/solr/server/solr/configsets/arclight
---
+++ solr-replication-config.py
@@ -0,0 +1,54 @@
+"""
+Update solrconfig.xml for index replication
+See https://solr.apache.org/guide/solr/latest/deployment-guide/user-managed-index-replication.html
+
+This script is pretty crude, and simply inserts the relevant configuration before the last line
+of the solrconfig.xml file
+"""
+
+import os
+
+solrconfig_path = "/opt/solr/server/solr/configsets/arclight/conf/solrconfig.xml"
+with open(solrconfig_path, "r") as f:
+    solr_config = [line for line in f]
+
+if solr_config[-1].strip() == "":
+    solr_config = solr_config[-1]
+
+if solr_config[-1].strip() != "</config>":
+    raise ValueError("Last line of solrconfig.xml is not '</config>'")
+
+# insert leader/follower config
+if os.environ.get("REPLICATION_ROLE") == "leader":
+    leader_config = [
+        "  <!-- leader configuration for index replication -->\n"
+        '  <requestHandler name="/replication" class="solr.ReplicationHandler">\n',
+        '    <lst name="leader">\n',
+        '      <str name="replicateAfter">optimize</str>\n',
+        '      <str name="backupAfter">optimize</str>\n',
+        '      <str name="confFiles">_rest_managed.json,elevate.xml,mapping-ISOLatin1Accent.txt,protwords.txt,schema.xml,scripts.conf,spellings.txt,stopwords_en.txt,stopwords.txt,synonyms.txt</str>\n',
+        "    </lst>\n",
+        '    <int name="maxNumberOfBackups">2</int>\n',
+        '    <str name="commitReserveDuration">00:00:10</str>\n',
+        '    <lst name="invariants">\n',
+        '      <str name="maxWriteMBPerSec">16</str>\n',
+        "    </lst>\n",
+        "  </requestHandler>\n\n",
+    ]
+    solr_config[-1:-1] = leader_config
+elif os.environ.get("REPLICATION_ROLE") == "follower":
+    solr_leader_url = os.environ["SOLR_LEADER_URL"]
+    follower_config = [
+        "  <!-- follower configuration for index replication -->\n"
+        '  <requestHandler name="/replication" class="solr.ReplicationHandler">\n',
+        '    <lst name="follower">\n',
+        f'      <str name="leaderUrl">{solr_leader_url}</str>\n',
+        '      <str name="pollInterval">00:00:20</str>\n',
+        "    </lst>\n",
+        "  </requestHandler>\n\n",
+    ]
+    solr_config[-1:-1] = follower_config
+
+with open(solrconfig_path, "w") as f:
+    for line in solr_config:
+        f.write(f"{line}")
