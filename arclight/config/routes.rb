bf75cf36a57deca4637aace7ee6c187884327f53
amy wieliczka <amywieliczka@gmail.com>
2025-06-02 12:36:47 -0600

    Arclight: add a map route, repositories action, and template

--- arclight/config/routes.rb
+++ arclight/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root to: "arclight/repositories#index"
   concern :searchable, Blacklight::Routes::Searchable.new
 
+  get "/map/", to: "arclight/repositories#map"
+
   resource :catalog, only: [], as: "catalog", path: "/catalog", controller: "catalog" do
     concerns :searchable
   end
