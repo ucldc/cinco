b2465d09366914f3940308c27640db0a19977a03
Barbara Hui <barbara.hui@ucop.edu>
2025-06-04 10:05:25 -0700

    Add contact_form, contact_mailer, and configure for SES

---
+++ arclight/app/controllers/contact_form_controller.rb
@@ -0,0 +1,12 @@
+class ContactFormController < ApplicationController
+    def create
+        @name = params[:contact_form][:name]
+        @last_name = params[:contact_form][:last_name]
+        @email = params[:contact_form][:email]
+        @message = params[:contact_form][:message]
+
+        # Perform any necessary actions with the form data
+        flash[:success] = "Your message has been sent successfully."
+        redirect_to :root
+    end
+end
---
+++ arclight/app/mailers/contact_mailer.rb
@@ -0,0 +1,8 @@
+class ContactMailer < ApplicationMailer
+    default from: "oac-feedback-l@ucop.edu"
+    default to: "oac-feedback-l@listserv.ucop.edu"
+
+    def test(email)
+        mail(:reply_to => email, :subject => "Hello World!")
+    end
+end
