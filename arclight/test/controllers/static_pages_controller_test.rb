d5ce7c92b757a8492b22de377f2e3d1c864c10b0
amy wieliczka <amywieliczka@gmail.com>
2025-06-06 12:05:41 -0700

    Arclight rename arclight/repositories#map to #home, remove static_files#home

--- arclight/test/controllers/static_pages_controller_test.rb
+++ arclight/test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,6 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
-
   test "should get about" do
     get static_pages_about_url
     assert_response :success
