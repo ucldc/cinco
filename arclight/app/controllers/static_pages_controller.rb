2774eb01bcff4647bcdbd94ebc71e4deb5360286
amy wieliczka <amywieliczka@gmail.com>
2025-06-06 12:51:50 -0700

    Arclight: create footer, stub out static pages

--- arclight/app/controllers/static_pages_controller.rb
+++ arclight/app/controllers/static_pages_controller.rb
@@ -2,6 +2,18 @@ class StaticPagesController < ApplicationController
   def about
   end
 
+  def faqs
+  end
+
   def help
+  end
+
+  def termsofuse
+  end
+
+  def privacy
+  end
+
+  def accessibility
   end
 end
