82263c41e550b1b59d146bb99f5db37534f9da16
Barbara Hui <barbara.hui@ucop.edu>
2025-06-09 14:57:06 -0700

    Basic working contact form

--- arclight/app/controllers/contact_form_controller.rb
+++ arclight/app/controllers/contact_form_controller.rb
@@ -1,11 +1,13 @@
 class ContactFormController < ApplicationController
     def create
-        @name = params[:contact_form][:name]
-        @last_name = params[:contact_form][:last_name]
-        @email = params[:contact_form][:email]
+        @subject = params[:contact_form][:subject]
         @message = params[:contact_form][:message]
+        @user_category = params[:contact_form][:user_category]
+        @user_category_other_description = params[:contact_form][:user_category_other_description]
+        @sender_name = params[:contact_form][:sender_name]
+        @sender_email = params[:contact_form][:sender_email]
 
-        # Perform any necessary actions with the form data
+        ContactMailer.contact_email(@subject, @message, @user_category, @user_category_other_description, @sender_name, @sender_email).deliver_now
         flash[:success] = "Your message has been sent successfully."
         redirect_to :root
     end
--- arclight/app/mailers/contact_mailer.rb
+++ arclight/app/mailers/contact_mailer.rb
@@ -1,8 +1,20 @@
 class ContactMailer < ApplicationMailer
     default from: "oac-feedback-l@ucop.edu"
-    default to: "oac-feedback-l@listserv.ucop.edu"
+    #default to: "oac-feedback-l@listserv.ucop.edu"
+    default to: "barbara.hui@ucop.edu"
 
-    def test(email)
-        mail(reply_to: email, subject: "Hello World!")
+    def contact_email(subject, message, user_category, user_category_other_description, sender_name, sender_email)
+        formatted_message = "Subject: #{subject}\n\n" \
+            "Message: #{message}\n\n" \
+            "User category: #{user_category}\n\n" \
+            "Other description: #{user_category_other_description}\n\n" \
+            "Sender name: #{sender_name}\n\n" \
+            "Sender email: #{sender_email}\n\n"
+
+        mail(
+            reply_to: sender_email,
+            subject: subject,
+            body: formatted_message
+        )
     end
 end
