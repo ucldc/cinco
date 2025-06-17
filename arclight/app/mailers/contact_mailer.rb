231d4ca65054e1292bf6ecf1a79838ca32d675a6
Barbara Hui <barbara.hui@ucop.edu>
2025-06-17 10:20:38 -0700

    Add contact page form and mailer (#431)
    * Add contact_form, contact_mailer, and configure for SES
    * Add docker-related files for local development

---
+++ arclight/app/mailers/contact_mailer.rb
@@ -0,0 +1,19 @@
+class ContactMailer < ApplicationMailer
+    default from: "oac-feedback-l@ucop.edu"
+    default to: "oac-feedback-l@listserv.ucop.edu"
+
+    def contact_email(subject, message, user_category, user_category_other_description, sender_name, sender_email)
+        formatted_message = "Subject: #{subject}\n\n" \
+            "Message: #{message}\n\n" \
+            "User category: #{user_category}\n\n" \
+            "Other description: #{user_category_other_description}\n\n" \
+            "Sender name: #{sender_name}\n\n" \
+            "Sender email: #{sender_email}\n\n"
+
+        mail(
+            reply_to: sender_email,
+            subject: "[OAC Feedback] #{subject}",
+            body: formatted_message
+        )
+    end
+end
