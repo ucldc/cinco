d0865801840b33931e19f29b1bbe58056eab6b02
amy wieliczka <amywieliczka@gmail.com>
2025-06-20 10:58:50 -0700

    Arclight: Update contact form fields

--- arclight/app/controllers/contact_form_controller.rb
+++ arclight/app/controllers/contact_form_controller.rb
@@ -1,13 +1,11 @@
 class ContactFormController < ApplicationController
     def create
-        @subject = params[:contact_form][:subject]
         @message = params[:contact_form][:message]
-        @user_category = params[:contact_form][:user_category]
-        @user_category_other_description = params[:contact_form][:user_category_other_description]
         @sender_name = params[:contact_form][:sender_name]
         @sender_email = params[:contact_form][:sender_email]
+        @sender_verify_email = params[:contact_form][:sender_verify_email]
 
-        ContactMailer.contact_email(@subject, @message, @user_category, @user_category_other_description, @sender_name, @sender_email).deliver_now
+        ContactMailer.contact_email(@message, @sender_name, @sender_email, @sender_verify_email).deliver_now
         flash[:success] = "Your message has been sent successfully."
         redirect_to :root
     end
--- arclight/app/mailers/contact_mailer.rb
+++ arclight/app/mailers/contact_mailer.rb
@@ -2,17 +2,15 @@ class ContactMailer < ApplicationMailer
     default from: "oac-feedback-l@ucop.edu"
     default to: "oac-feedback-l@listserv.ucop.edu"
 
-    def contact_email(subject, message, user_category, user_category_other_description, sender_name, sender_email)
-        formatted_message = "Subject: #{subject}\n\n" \
-            "Message: #{message}\n\n" \
-            "User category: #{user_category}\n\n" \
-            "Other description: #{user_category_other_description}\n\n" \
+    def contact_email(message, sender_name, sender_email, sender_verify_email)
+        formatted_message = "Message: #{message}\n\n" \
             "Sender name: #{sender_name}\n\n" \
-            "Sender email: #{sender_email}\n\n"
+            "Sender email: #{sender_email}\n\n" \
+            "Sender verify email: #{sender_verify_email}\n\n"
 
         mail(
             reply_to: sender_email,
-            subject: "[OAC Feedback] #{subject}",
+            subject: "[OAC Feedback] #{sender_name} has sent a message",
             body: formatted_message
         )
     end
