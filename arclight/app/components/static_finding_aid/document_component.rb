103c7cdee4917c31c23c5625f6c354c79f60cc99
Chad Nelson <chadbnelson@gmail.com>
2025-06-20 16:13:12 -0500

    Somewhat working static pages

---
+++ arclight/app/components/static_finding_aid/document_component.rb
@@ -0,0 +1,15 @@
+module StaticFindingAid
+  class DocumentComponent < Arclight::DocumentComponent
+    def initialize(document: nil, presenter: nil, partials: nil,
+      id: nil, classes: [], component: :article, title_component: nil,
+      counter: nil, document_counter: nil, counter_offset: 0,
+      show: false, doc_tree: nil, **args)
+      super
+      @document_tree = doc_tree
+    end
+
+    def document_tree
+      @document_tree
+    end
+  end
+end
---
+++ arclight/app/components/static_finding_aid/metadata_section_component.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module StaticFindingAid
+  # Render a simple metadata field (e.g. without labels) in a .row div
+  class MetadataSectionComponent < Arclight::MetadataSectionComponent
+  end
+end
