13b236cc7b195f8cd6d99b5aacf932f336a41e48
Chad Nelson <chadbnelson@gmail.com>
2025-06-22 16:38:47 -0500

    Move TreeNode impl to lib; add tests

--- arclight/app/controllers/static_finding_aid_controller.rb
+++ arclight/app/controllers/static_finding_aid_controller.rb
@@ -1,3 +1,5 @@
+require "oac/finding_aid_tree_node"
+
 class StaticFindingAidController < CatalogController
   layout "static_catalog_result"
 
@@ -8,34 +10,7 @@ class StaticFindingAidController < CatalogController
   end
 
   def show
-    @doc_tree = FindingAidTreeNode.new(self, params[:id])
+    @doc_tree = Oac::FindingAidTreeNode.new(self, params[:id])
     @document = @doc_tree.document
-  end
-end
-
-
-class FindingAidTreeNode
-  attr_reader :document, :hierarchy_data
-
-  def initialize(controller, id, has_children: true)
-    @controller = controller
-    @document = controller.search_service.fetch(id)
-    default_solr_params = @controller.blacklight_config.default_solr_params.dup
-    @controller.blacklight_config.default_solr_params.merge!(
-      { fq: "_nest_parent_:#{id}", sort: "sort_isi asc", facet: false }
-    )
-    @hierarchy_data = @controller.search_service.search_results.response["docs"].map { |doc| SolrDocument.new(doc) } if has_children || []
-    @controller.blacklight_config.default_solr_params = default_solr_params
-    children
-  end
-
-  def children
-    @children ||= @hierarchy_data.map do  |doc|
-      FindingAidTreeNode.new(
-        @controller,
-        doc.id,
-        has_children: doc.fetch("child_component_count_isi", 0) > 0
-      )
-    end
   end
 end
---
+++ arclight/spec/lib/oac/finding_aid_tree_node_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+require 'oac/finding_aid_tree_node'
+
+RSpec.describe Oac::FindingAidTreeNode do
+  let(:tree) { described_class.new(controller, id) }
+  let(:controller) { instance_double(StaticFindingAidController) }
+  let(:id) { 'test_id' }
+  let(:first_hierarchy_response) {
+    ActiveSupport::HashWithIndifferentAccess.new(
+      { "docs": [ { id: 1 }, { id: 2 }, { id: 3 } ] })
+  }
+  let(:second_hierarchy_response) {
+    ActiveSupport::HashWithIndifferentAccess.new(
+      { "docs": [ { id: 4 }, { id: 5 }, { id: 6 } ] }
+    )
+  }
+  let(:empty_hierarchy_response) {
+    ActiveSupport::HashWithIndifferentAccess.new(
+      { "docs": [] }
+    )
+  }
+
+  let(:default_solr_params) {
+    { default: "params" }
+  }
+
+  before do
+    allow(controller).to receive(:search_service)
+    allow(controller.search_service).to receive(:fetch)
+    allow(controller.search_service).to receive(:search_results)
+    allow(controller.search_service.search_results).to receive(:response).and_return(
+      first_hierarchy_response,
+      second_hierarchy_response,
+      empty_hierarchy_response
+    )
+
+    allow(controller).to receive(:blacklight_config)
+
+    allow(controller.blacklight_config).to receive(:default_solr_params).and_return(default_solr_params)
+    allow(controller.blacklight_config).to receive(:default_solr_params=)
+    allow(controller.blacklight_config.default_solr_params).to receive(:merge!)
+  end
+
+  describe "creating an instance" do
+    before { tree }
+    it "calls the controller's search service fetch method" do
+      expect(controller.search_service).to have_received(:fetch).with(id)
+    end
+    it "temporarily merge's some default_solr_params" do
+      expect(controller.blacklight_config.default_solr_params).to have_received(:merge!).with({ fq: "_nest_parent_:#{id}", sort: "sort_isi asc", facet: false })
+    end
+    it "leaves the default_solr_params in their original state" do
+      expect(controller.blacklight_config).to have_received(:default_solr_params=).with(default_solr_params).at_least(:once)
+    end
+  end
+  describe "children method" do
+    before { tree }
+    it "returns an array of #{described_class.name} objects" do
+      expect(tree.children.map { |c| c.class }.uniq).to be_eql([ described_class ])
+    end
+    it "nests children" do
+      expect(tree.children.first.children.first).to be_a(described_class)
+    end
+  end
+end
