7fd6f535acc087d7a14e927a7391b5047ef3d031
amy wieliczka <amywieliczka@gmail.com>
2025-07-02 11:29:10 -0700

    Arclight: add Matomo debugging, not sure turbo:load is the right event

--- arclight/app/javascript/application.js
+++ arclight/app/javascript/application.js
@@ -6,3 +6,46 @@ import githubAutoCompleteElement from "@github/auto-complete-element"
 import Blacklight from "blacklight"
 
 import "arclight"
+
+
+// Debugging Matomo tracking
+
+class _paq {
+    static push(...args) {
+        console.log("Matomo tracking:", ...args);
+    }
+}
+
+// function to track a page view with Matomo
+function trackMatomoPageView() {
+    if (typeof _paq !== 'undefined') {
+        console.log("Tracking page view with Matomo");
+        _paq.push(['setReferrerUrl', document.referrer]);
+        _paq.push(['setCustomUrl', location.href]);
+        _paq.push(['setDocumentTitle', document.title]);
+
+        // remove all previously assigned custom dimensions
+        _paq.push(['deleteCustomDimension', 1]);
+        _paq.push(['deleteCustomDimension', 2]);
+
+        // add custom dimensions, if available
+        var repoDimension = document.getElementById('matomo-repo');
+        if (repoDimension) {
+            _paq.push(['setCustomDimension', 1, repoDimension.getAttribute('data-repo-slug')]);
+        }
+        var collectionDimension = document.getElementById('matomo-collection');
+        if (collectionDimension) {
+            _paq.push(['setCustomDimension', 2, collectionDimension.getAttribute('data-ark')]);
+        }
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+    }
+}
+
+document.documentElement.addEventListener('turbo:load', function() {
+    return function(e) {
+        console.log(e);
+        console.log("document.location: ", document.location);
+        trackMatomoPageView();
+    };
+}());
