14ab989e256d60d4660642e9dccd03d72e724a97
amy wieliczka <amywieliczka@gmail.com>
2025-07-02 17:44:10 -0700

    Arclight contact form: add referral URL

--- arclight/app/controllers/contact_form_controller.rb
+++ arclight/app/controllers/contact_form_controller.rb
@@ -4,8 +4,12 @@ class ContactFormController < ApplicationController
         @sender_name = params[:contact_form][:sender_name]
         @sender_email = params[:contact_form][:sender_email]
         @sender_verify_email = params[:contact_form][:sender_verify_email]
+        @referrer = params[:contact_form][:referrer]
+        if @referrer.blank?
+            @referrer = "No referrer"
+        end
 
-        ContactMailer.contact_email(@message, @sender_name, @sender_email, @sender_verify_email).deliver_now
+        ContactMailer.contact_email(@message, @sender_name, @sender_email, @sender_verify_email, @referrer).deliver_now
         flash[:success] = "Your message has been sent successfully."
         redirect_to :root
     end
