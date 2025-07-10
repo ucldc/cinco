d2b1cdfaa9952a0df54542a26dd76ded73a17362
Barbara Hui <barbara.hui@ucop.edu>
2025-07-10 14:48:05 -0700

    Cincoctrl send pwd reset emails (#580)
    * Add IAM ses permissions template
    
    * Update stage and prod email settings

--- cincoctrl/config/settings/production.py
+++ cincoctrl/config/settings/production.py
@@ -126,14 +126,14 @@ STATIC_URL = f"https://{aws_s3_domain}/static/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="cincoctrl <noreply@oac5.cdlib.org>",
+    default="OAC Operations <oacops@cdlib.org>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = env(
     "DJANGO_EMAIL_SUBJECT_PREFIX",
-    default="[cincoctrl] ",
+    default="[OAC] ",
 )
 ACCOUNT_EMAIL_SUBJECT_PREFIX = EMAIL_SUBJECT_PREFIX
 
