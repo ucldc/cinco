b6f158ea4ce3c95ed146532bd76a2542df3d31f1
amy wieliczka <amywieliczka@gmail.com>
2025-07-21 10:54:37 -0700

    Update prod with functional stage settings [cincoctrl]

--- cincoctrl/config/settings/production.py
+++ cincoctrl/config/settings/production.py
@@ -20,9 +20,11 @@ AIRFLOW_PROJECT_NAME = "cinco-prd"
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
+    "dashboard.oac.cdlib.org",
     "oac.cdlib.org",
     "*",
 ]
+CSRF_TRUSTED_ORIGINS = ["https://dashboard.oac.cdlib.org"]
 
 # We need the ALB's DNS record & public IP in ALLOWED_HOSTS, but neither
 # are persistent at the moment - revisit once we have a persistent DNS
@@ -57,13 +59,13 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
 SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=False)
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
-# SESSION_COOKIE_SECURE = True  # TODO: AW - uncomment this once you have enabled HTTPS
+SESSION_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-name
-# SESSION_COOKIE_NAME = "__Secure-sessionid"  # TODO: AW - uncomment this once you have enabled HTTPS
+SESSION_COOKIE_NAME = "__Secure-sessionid"
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
-# CSRF_COOKIE_SECURE = True  # TODO: AW - uncomment this once you have enabled HTTPS
+CSRF_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-name
-# CSRF_COOKIE_NAME = "__Secure-csrftoken"  # TODO: AW - uncomment this once you have enabled HTTPS
+CSRF_COOKIE_NAME = "__Secure-csrftoken"
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
