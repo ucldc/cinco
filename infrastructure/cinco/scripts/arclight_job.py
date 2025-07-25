2245c91d04ea96f021e751a27d1a4ad8ab5e84a2
amy wieliczka <amywieliczka@gmail.com>
2025-07-24 18:08:45 -0700

    Add arclight_job docs [infra]

--- infrastructure/cinco/scripts/arclight_job.py
+++ infrastructure/cinco/scripts/arclight_job.py
@@ -1,7 +1,59 @@
-import argparse
-# import time
+"""
+Requires a python3 environment w/ boto3 installed, cdl-pad-prd profile
+configured (as per cdl-ssm-util), and AWS_* environment variables.
 
+Runs an ephemeral arclight worker container (outside the ALB) in the
+cinco-stage cluster (by default). Pass --prd to run an arclight worker
+container in the cinco-prd cluster.
+
+Prints the Task ARN, an aws cli command to tail the logs, an aws cli
+command to start an interactive session on the container, and an aws cli
+command to stop the container. It does take ~ a minute for the task to
+become accessible via `ecs execute-command` (or via the `session`
+utility).
+
+By default, the arclight worker container is defined by the same task
+definition as the currently running service definition (ie: if the
+cinco-arclight-stage-service is running task definition verion 6, then
+this script will run a task using task definition version 6).
+
+You can also explicitly specify a task definition revision by passing
+--task-definition <revision number>, or use the --latest flag to run the
+most recent active task definition revision. (Explicitly specifying
+revision number is useful when chained together with task definition
+updates in the build pipeline, for example.)
+
+You must specify a command to run on the ephemeral container.
+
+usage examples:
+
+to run a rake command in cinco-stage cluster:
+    python arclight_job.py rake ...
+
+to manually index from s3 in cinco-prd cluster:
+    python arclight_job.py --prd bin/index-from-s3 <finding_aid_id> <s3_key> <repository_code> <finding_aid_ark> [--preview]"
+
+to build static finding aids in cinco-prd cluster:
+    python arclight_job.py --prd bin/build-static-findaids
+
+to examine and explore the filesystem for an older arclight version:
+    python arclight_job.py --task-definition 4 sh -c "tail -f /dev/null"
+"""
+
+import argparse
 import boto3
+
+
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
 
 
 def task_template():
@@ -13,6 +65,7 @@ def task_template():
         "platformVersion": "LATEST",
         "enableECSManagedTags": True,
         "enableExecuteCommand": True,
+        "startedBy": "arclight_job.py",
     }
 
 
@@ -74,7 +127,7 @@ def main(
     )
     network_configuration = get_service_network_config(cluster, env)
 
-    print(f"Running `{' '.join(command)}` on Cinco Arclight {env} in ECS")
+    print(f"Running `{' '.join(command)}` on Cinco Arclight {env} in ECS\n")
 
     ecs_client = boto3.client("ecs", region_name="us-west-2")
     resp = ecs_client.run_task(
@@ -93,30 +146,34 @@ def main(
         },
     )
     task_arn = [task["taskArn"] for task in resp["tasks"]][0]
-    print(f"Started task: {task_arn}")
+    print(
+        f"{bcolors.HEADER}Started task:{bcolors.ENDC} {bcolors.BOLD}{task_arn}{bcolors.ENDC}\n"
+    )
 
     task_id = task_arn.split("/")[-1]
     log_group_name = f"/ecs/cinco-arclight-{stack}"
     log_stream_name = f"ecs/cinco-arclight-{stack}-container/{task_id}"
     print(
-        "Tail logs with command:\n"
+        f"{bcolors.HEADER}Tail logs with command:{bcolors.ENDC}\n"
         f"aws logs tail {log_group_name} --log-stream-name-prefix "
-        f"{log_stream_name} --region us-west-2"
+        f"{log_stream_name}\n"
     )
 
     print(
-        f"Session to the machine with command: (assuming you have a cdl-pad-prd profile)\n"
+        f"{bcolors.HEADER}Session to the machine with command: (assuming you have a cdl-pad-prd profile){bcolors.ENDC}\n"
         f"aws ecs execute-command --profile cdl-pad-prd "
         f"--cluster arn:aws:ecs:us-west-2:777968769372:cluster/cinco-{env} "
         f"--task {task_arn} "
         f"--container cinco-arclight-{env}-container "
-        "--command /bin/bash --interactive"
+        f"--command /bin/bash --interactive\n"
+    )
+
+    print(
+        f"{bcolors.HEADER}Stop container with command:{bcolors.ENDC}\n"
+        f"aws ecs stop-task --cluster cinco-{env} --task {task_arn}\n"
     )
 
 
-# python arclight_job.py rake
-# python arclight_job.py bin/generate_static_pages
-# python arclight_job.py sh -c tail -f /dev/null
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Run commands on an Arclight ECS instance"
@@ -146,6 +203,3 @@ if __name__ == "__main__":
         parser.error("You must provide a command to run.")
 
     main(stack, args.command, args.task_definition, latest)
-    # tail_logs = main(stack, args.command, args.task_definition)
-    # print(f"{bcolors.OKCYAN}[ECS_MANAGE]: {tail_logs}{bcolors.ENDC}")
-    # os.system(tail_logs)
