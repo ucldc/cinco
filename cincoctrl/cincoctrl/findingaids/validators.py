01e1cd58d7b7a12911a656e5ec067cec12e92591
Esther Verreau <esther.verreau@ucop.edu>
2025-07-25 13:10:36 -0500

    move component checking out of main validator and into warnings

--- cincoctrl/cincoctrl/findingaids/validators.py
+++ cincoctrl/cincoctrl/findingaids/validators.py
@@ -9,7 +9,6 @@ def validate_ead(file):
         parser = EADParser()
         parser.parse_file(file)
         parser.validate_required_fields()
-        parser.validate_component_titles()
         if len(parser.errors) > 0:
             # TODO: can we include all the error messages?
             raise ValidationError(parser.errors[0]) from None
