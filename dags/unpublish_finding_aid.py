db45c37e74e0d307a8cecca0afc66bbd92e28891
Amy Wieliczka <amywieliczka@gmail.com>
2025-09-02 12:23:14 -0700

    Add Unpublish Finding Aid dag [dags]

---
+++ dags/unpublish_finding_aid.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from airflow.decorators import dag
+from airflow.models.param import Param
+
+# from cinco.cincoctrl_operator import CincoCtrlOperator
+from cinco.arclight_operator import ArcLightOperator
+
+
+@dag(
+    dag_id="unpublish_finding_aid",
+    schedule=None,
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    params={
+        "finding_aid_ark": Param(
+            "", type="string", description="The ARK of the Finding Aid"
+        ),
+        "cinco_environment": Param(
+            "stage",
+            enum=["stage", "prd"],
+            description="The CincoCtrl and ArcLight environment to run",
+        ),
+    },
+    tags=["cinco"],
+    # on_failure_callback=notify_dag_failure,
+    # on_success_callback=notify_dag_success,
+)
+def unpublish_finding_aid():
+    remove_from_index = ArcLightOperator(
+        task_id="remove_from_index",
+        arclight_command="remove-from-solr",
+        finding_aid_ark="{{ params.finding_aid_ark }}",
+        cinco_environment="{{ params.cinco_environment }}",
+    )
+    remove_from_index
+
+    # mark_unpublished = CincoCtrlOperator(
+    #     task_id="mark_unpublished",
+    #     manage_cmd="mark_unpublished",
+    #     finding_aid_ark="{{ params.finding_aid_ark }}",
+    #     cinco_environment="{{ params.cinco_environment }}",
+    # )
+
+    # remove_from_index >> mark_unpublished
+
+
+unpublish_finding_aid = unpublish_finding_aid()
