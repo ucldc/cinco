c610adc29cc970e516abbd3afd29b1c81a6ab6d6
Amy Wieliczka <amywieliczka@gmail.com>
2025-09-03 10:28:30 -0700

    s3 key input should be relative removals/ path [cincoctrl]

--- cincoctrl/cincoctrl/findingaids/management/commands/bulk_remove_finding_aids.py
+++ cincoctrl/cincoctrl/findingaids/management/commands/bulk_remove_finding_aids.py
@@ -14,14 +14,17 @@ class Command(BaseCommand):
         parser.add_argument(
             "--s3_key",
             help="""The s3_key where a csv of 'finding aid ark, repository code' pairs
-                    to be deleted are stored""",
+                    to be deleted are stored (relative to removals/)""",
             type=str,
         )
 
     def handle(self, *args, **options):
         s3_key = options.get("s3_key")
         s3_client = boto3.client("s3")
-        obj = s3_client.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=s3_key)
+        obj = s3_client.get_object(
+            Bucket=settings.AWS_STORAGE_BUCKET_NAME,
+            Key=f"removals/{s3_key}",
+        )
         body = obj["Body"].read().decode("utf-8")
         for line in body.splitlines():
             ark, repo_code = line.split(",")
