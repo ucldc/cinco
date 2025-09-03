059107daceedb1522fac8edfb71fd14409fdf260
Amy Wieliczka <amywieliczka@gmail.com>
2025-09-02 18:00:16 -0700

    Bulk remove finding aids from cincoctrl [cincoctrl]

---
+++ cincoctrl/cincoctrl/findingaids/management/commands/bulk_remove_finding_aids.py
@@ -0,0 +1,31 @@
+import boto3
+from django.core import settings
+from django.core.management.base import BaseCommand
+
+from cincoctrl.findingaids.models import FindingAid
+
+
+class Command(BaseCommand):
+    """Bulk remove finding aids"""
+
+    help = "Bulk remove finding aids"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--s3_key",
+            help="""The s3_key where a csv of 'finding aid ark, repository code' pairs
+                    to be deleted are stored""",
+            type=str,
+        )
+
+    def handle(self, *args, **options):
+        s3_key = options.get("s3_key")
+        s3_client = boto3.client("s3")
+        obj = s3_client.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=s3_key)
+        body = obj["Body"].read().decode("utf-8")
+        for line in body.splitlines():
+            ark, repo_code = line.split(",")
+            ark = ark.strip()
+            f = FindingAid.objects.get(ark=ark)
+            f.delete()
+            self.stdout.write(f"Deleted finding aid {ark}")
