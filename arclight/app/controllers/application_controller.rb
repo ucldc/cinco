eed1dd542d4999381da6a379c6d444154646484a
Amy Wieliczka <amywieliczka@gmail.com>
2025-09-17 15:16:41 -0700

    Add some temporary logging to debug 406s [arclight]

--- arclight/app/controllers/application_controller.rb
+++ arclight/app/controllers/application_controller.rb
@@ -6,4 +6,18 @@ class ApplicationController < ActionController::Base
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # temporarily log accept headers and rails-determined formats
+  # to debug 406 errors TODO: remove after debugging
+  before_action :log_accept_and_format
+
+  private
+  def log_accept_and_format
+    Rails.logger.info({
+      accept: request.headers["Accept"],
+      requested_format: request.format&.to_s,
+      params_format: params[:format],
+      path: request.fullpath
+    }.to_json)
+  end
 end
