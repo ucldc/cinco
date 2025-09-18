f4736ecca13dfc1e07162c9665a2f2f5ca1521b6
Amy Wieliczka <amywieliczka@gmail.com>
2025-09-18 14:02:38 -0700

    Add request accept header to log message [arclight]

--- arclight/config/initializers/logging.rb
+++ arclight/config/initializers/logging.rb
@@ -5,9 +5,11 @@ Rails.application.configure do
   config.lograge.enabled = true if Rails.env.production?
 
   config.lograge.custom_options = lambda do |event|
+      request = event.payload[:request]
       {
         time: Time.now,
-        query_string: event.payload[:query_string]
+        query_string: event.payload[:query_string],
+        accept: request&.headers["Accept"]
       }
   end
 
