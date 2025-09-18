58b357eb9a023387da931afecd2945954374f19c
Amy Wieliczka <amywieliczka@gmail.com>
2025-09-18 11:44:22 -0700

    Add query string to log messages [arclight]

--- arclight/config/initializers/logging.rb
+++ arclight/config/initializers/logging.rb
@@ -5,7 +5,10 @@ Rails.application.configure do
   config.lograge.enabled = true if Rails.env.production?
 
   config.lograge.custom_options = lambda do |event|
-      { time: Time.now }
+      {
+        time: Time.now,
+        query_string: event.payload[:query_string]
+      }
   end
 
     config.lograge.formatter = Lograge::Formatters::Json.new
