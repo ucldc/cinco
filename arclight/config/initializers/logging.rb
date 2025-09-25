db3ad1d81b741c534bbbdd3097eab4a93600314f
Amy Wieliczka <amywieliczka@gmail.com>
2025-09-25 16:06:11 -0700

    path is not actually the request path, add it [arclight]

--- arclight/config/initializers/logging.rb
+++ arclight/config/initializers/logging.rb
@@ -11,7 +11,8 @@ Rails.application.configure do
         accept_header: request&.headers["Accept"],
         user_agent: request&.headers["User-Agent"],
         cloudfront_request_id: request&.headers["X-Amz-Cf-Id"],
-        rails_request_id: request&.request_id
+        rails_request_id: request&.request_id,
+        originally_requested_path: request&.original_fullpath
       }
   end
 
