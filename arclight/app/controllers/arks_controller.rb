00ca6e422dd170edfcc0c5cda83b3372e71d9270
Amy Wieliczka <amywieliczka@gmail.com>
2025-09-25 12:28:35 -0700

    Add user agent to logging output [arclight]

--- arclight/app/controllers/arks_controller.rb
+++ arclight/app/controllers/arks_controller.rb
@@ -1,10 +1,38 @@
 class ArksController < ApplicationController
   def findaid
-    redirect_to solr_document_url(params[:ark].to_param)
+    # log the URL we are redirecting to
+    url = solr_document_url(params[:ark].to_param)
+    Rails.logger.info({
+      event: "Redirecting to Solr Document URL",
+      solr_document_url: url,
+      rails_request_id: request.uuid,
+      format: request.format.symbol,
+      path: request.path,
+      controller: self.class.name,
+      action: action_name,
+      cloudfront_request_id: request.headers["X-Amz-Cf-Id"],
+      accept_header: request.headers["Accept"],
+      user_agent: request.headers["User-Agent"]
+    }.to_json)
+    redirect_to url
   end
 
   def findaid_static
-    redirect_to static_finding_aid_url(params[:ark].to_param)
+    # log the URL we are redirecting to
+    url = static_finding_aid_url(params[:ark].to_param)
+    Rails.logger.info({
+      event: "Redirecting to Static Finding Aid URL",
+      static_finding_aid_url: url,
+      rails_request_id: request.uuid,
+      format: request.format.symbol,
+      path: request.path,
+      controller: self.class.name,
+      action: action_name,
+      cloudfront_request_id: request.headers["X-Amz-Cf-Id"],
+      accept_header: request.headers["Accept"],
+      user_agent: request.headers["User-Agent"]
+    }.to_json)
+    redirect_to url
   end
 
   def calisphere
