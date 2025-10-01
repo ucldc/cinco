f2da09d0b8f56e4e0512cb080c31e491ccb40ea8
Amy Wieliczka <amywieliczka@gmail.com>
2025-10-01 15:14:04 -0700

    Allow arks with oac_static_finding_aid_path; fix static_finding_aid_path urls

--- arclight/app/helpers/static_finding_aid_helper.rb
+++ arclight/app/helpers/static_finding_aid_helper.rb
@@ -11,4 +11,18 @@ module StaticFindingAidHelper
 
   def link_to_name_facet(args)
   end
+
+  def oac_static_finding_aid_path(*args, **options)
+    document_id = options[:id] || (args.first.respond_to?(:id) ? args.first.id : args.first)
+
+    encoded_path = static_finding_aid_path(*args, **options)
+    Rails.logger.info("Generated path from original: #{encoded_path}")
+
+    if document_id.to_s.start_with?("ark:")
+      Rails.logger.info("ARK detected, generating custom static finding aid path.")
+      encoded_path.gsub!("%2F", "/")
+      Rails.logger.info("Rewritten static finding aid path: #{encoded_path}")
+    end
+    encoded_path
+  end
 end
