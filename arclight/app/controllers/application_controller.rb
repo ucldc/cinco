f537b12cc247c2d1bb30810943f08e0a2d8a7fff
Amy Wieliczka <amywieliczka@gmail.com>
2025-10-01 16:03:28 -0700

    Overwrite rails generated solr_document_path, instead of writing custom helper

--- arclight/app/controllers/application_controller.rb
+++ arclight/app/controllers/application_controller.rb
@@ -6,4 +6,20 @@ class ApplicationController < ActionController::Base
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :solr_document_path
+
+  def solr_document_path(*args, **options)
+    Rails.logger.info("solr_document_path called with args: #{args}, options: #{options}")
+    document_id = options[:id] || (args.first.respond_to?(:id) ? args.first.id : args.first)
+    encoded_path = super(*args, **options)
+    Rails.logger.info("Generated path from original: #{encoded_path}")
+
+    if document_id.to_s.start_with?("ark:")
+      Rails.logger.info("ARK detected, generating custom solr document path.")
+      encoded_path.gsub!("%2F", "/")
+      Rails.logger.info("Rewritten solr document path: #{encoded_path}")
+    end
+    encoded_path
+  end
 end
