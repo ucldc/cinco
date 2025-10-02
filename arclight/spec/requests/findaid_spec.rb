1c1afaed65da0190ec531fcc1d2550914845b22f
Amy Wieliczka <amywieliczka@gmail.com>
2025-10-02 13:22:08 -0700

    Move entire_text redirect into routes, out of arks controller

--- arclight/spec/requests/findaid_spec.rb
+++ arclight/spec/requests/findaid_spec.rb
@@ -4,6 +4,30 @@
 require 'rails_helper'
 
 RSpec.describe "OAC-4 style findaid redirects", type: :request do
+  describe "GET /findaid/ark:/13010/sdfsdfsf/entire_text" do
+    it "returns http redirect" do
+      get "/findaid/ark:/13010/sdfsdfsf/entire_text"
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to("/findaid/static/ark:/13010/sdfsdfsf")
+    end
+  end
+
+  describe "GET /findaid/ark:/13010/sdfsdfsf/entire_text/" do
+    it "returns http redirect" do
+      get "/findaid/ark:/13010/sdfsdfsf/entire_text/"
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to("/findaid/static/ark:/13010/sdfsdfsf")
+    end
+  end
+
+  describe "GET /findaid/sdfsdfsf/entire_text" do
+    it "returns http redirect for non-ark finding aids" do
+      get "/findaid/sdfsdfsf/entire_text"
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to("/findaid/static/sdfsdfsf")
+    end
+  end
+
   describe "GET /findaid/ark:/13010/sdfsdfsf/admin" do
     it "returns http redirect" do
       get "/findaid/ark:/13010/sdfsdfsf/admin"
