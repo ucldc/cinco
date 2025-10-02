6dbb2fd0c08dd5eb941bf2630e0142102af9b745
Amy Wieliczka <amywieliczka@gmail.com>
2025-10-01 23:02:54 -0700

    No longer need ArksController findaid action

--- arclight/app/controllers/arks_controller.rb
+++ arclight/app/controllers/arks_controller.rb
@@ -1,23 +1,4 @@
 class ArksController < ApplicationController
-  def findaid
-    # log the URL we are redirecting to
-    url = solr_document_url(params[:ark].to_param)
-    Rails.logger.info({
-      event: "Redirecting to Solr Document URL",
-      solr_document_url: url,
-      rails_request_id: request.uuid,
-      format: request.format.symbol,
-      path: request.path,
-      controller: self.class.name,
-      action: action_name,
-      cloudfront_request_id: request.headers["X-Amz-Cf-Id"],
-      accept_header: request.headers["Accept"],
-      user_agent: request.headers["User-Agent"],
-      originally_requested_path: request&.original_fullpath
-    }.to_json)
-    redirect_to url
-  end
-
   def findaid_static
     # log the URL we are redirecting to
     url = static_finding_aid_url(params[:ark].to_param)
