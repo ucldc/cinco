87470d6f2808d2770c5669231918d673aa5bd82a
Amy Wieliczka <amywieliczka@gmail.com>
2025-10-03 11:18:35 -0700

    Copy the breadcrumb component from arclight so it uses our helpers

---
+++ arclight/app/components/arclight/breadcrumb_component.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module Arclight
+  # Display the document hierarchy as "breadcrumbs"
+  class BreadcrumbComponent < ViewComponent::Base
+    # @param [Integer] count if provided the number of bookmarks is limited to this number
+    def initialize(document:, count: nil, offset: 0)
+      @document = document
+      @count = count
+      @offset = offset
+      super
+    end
+
+    def call
+      breadcrumb_links = components.drop(@offset)
+
+      if @count && breadcrumb_links.length > @count
+        breadcrumb_links = breadcrumb_links.first(@count)
+        breadcrumb_links << tag.li("&hellip;".html_safe, class: "breadcrumb-item")
+      end
+      tag.ol class: "breadcrumb" do
+        safe_join(breadcrumb_links)
+      end
+    end
+
+    def components
+      return to_enum(:components) unless block_given?
+
+      yield build_repository_link
+
+      Rails.logger.debug("Breadcrumb parents: #{@document.parents.map(&:id).join(', ')}")
+
+      @document.parents.each do |parent|
+        yield tag.li(class: "breadcrumb-item") { link_to(parent.label, helpers.solr_document_path(parent.id)) }
+      end
+    end
+
+    def build_repository_link
+      render Arclight::RepositoryBreadcrumbComponent.new(document: @document)
+    end
+  end
+end
