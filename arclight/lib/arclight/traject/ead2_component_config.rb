1dbb893df55e3920651806014972435f4591ca41
Barbara Hui <barbara.hui@ucop.edu>
2025-11-12 15:37:01 -0800

    Add oac_normalized_title_ssm solr field mapping to traject component config

--- arclight/lib/arclight/traject/ead2_component_config.rb
+++ arclight/lib/arclight/traject/ead2_component_config.rb
@@ -140,6 +140,17 @@ to_field "normalized_title_ssm" do |_record, accumulator, context|
   accumulator << settings["title_normalizer"].constantize.new(title, date, container_label, unitid).to_s
 end
 
+to_field "oac_normalized_title_ssm" do |_record, accumulator, context|
+  oac_title = context.output_hash["title_filing_ssi"]&.first
+  if oac_title.blank?
+    oac_title = context.output_hash["title_ssm"]&.first
+  end
+  date = context.output_hash["normalized_date_ssm"]&.first
+  container_label = context.output_hash["containers_ssim"]
+  unitid = context.output_hash["unitid_ssm"]&.first
+  accumulator << settings["title_normalizer"].constantize.new(oac_title, date, container_label, unitid).to_s
+end
+
 to_field "component_level_isim" do |_record, accumulator|
   accumulator << (settings[:depth] || 1)
 end
