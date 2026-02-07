3ac88c43d2d5b6cbec6d4775c4b5e591b9f821f6
Amy Wieliczka <amywieliczka@gmail.com>
2026-02-06 20:25:42 -0800

    Cache static finding aids in s3 [arclight]

--- arclight/config/initializers/static_finding_aid.rb
+++ arclight/config/initializers/static_finding_aid.rb
@@ -8,3 +8,16 @@ Rails.application.config.child_component_limit = ENV["CHILD_COMPONENT_LIMIT"] ||
 Rails.application.config.disallowed_static_guides = [
   "ark:/13030/c8tt4pp0"
 ]
+
+# Configure AWS/MinIO for local development
+if ENV["CINCO_MINIO_ENDPOINT"].present?
+  Aws.config.update(
+    endpoint: ENV["CINCO_MINIO_ENDPOINT"],
+    access_key_id: "minioadmin",
+    secret_access_key: "minioadmin",
+    force_path_style: true,
+    region: "us-east-1"
+  )
+
+  ENV["S3_BUCKET"] ||= "cinco-dev"
+end
