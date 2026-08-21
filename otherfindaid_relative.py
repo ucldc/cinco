1ca7f7652d47e397e9b81855cdf87ae4fda73069
Barbara Hui <barbara.hui@ucop.edu>
2026-08-21 14:30:53 -0700

    Use s3 key when getting object, not filename!

--- otherfindaid_relative.py
+++ otherfindaid_relative.py
@@ -5,19 +5,24 @@ import xml.etree.ElementTree as ET
 s3_client = boto3.client("s3")
 paginator = s3_client.get_paginator('list_objects_v2')
 
+s3_bucket = "cinco-prd"
 s3_page = 0
-for page in paginator.paginate(Bucket='cinco-prd', Prefix='media/ead'):
+for page in paginator.paginate(Bucket=s3_bucket, Prefix='media/ead'):
     print(f"\n# s3 page: {s3_page}")
     eads_with_extrefs = {}
     if 'Contents' in page:
         for obj in page['Contents']:
             key = obj['Key']
-            filename = key.split("/")[-1]
 
-            response = s3_client.get_object(Bucket="cinco-prd", Key=f"media/ead/{filename}")
+            response = s3_client.get_object(Bucket=s3_bucket, Key=key)
             # print(f"{key=}")
             file_content = response['Body'].read().decode('utf-8', 'ignore')
             root = ET.fromstring(file_content)
+
+            # get ark
+            for eadid in root.iter('eadid'):
+                ark = eadid.attrib.get('identifier')
+
             parent_map = {child: parent for parent in root.iter() for child in parent}
 
             hrefs = []
@@ -42,12 +47,15 @@ for page in paginator.paginate(Bucket='cinco-prd', Prefix='media/ead'):
             for href in hrefs:
                 href = href["href"]
                 if href == None:
-                    print(f"**** {filename} has href of None: {hrefs}")
+                    print(f"**** {key} has href of None: {hrefs}")
                 elif not href.startswith("http"):
-                    eads_with_extrefs[filename] = {
+                    s3_url = f"https://{s3_bucket}.s3.us-west-2.amazonaws.com/{key}"
+                    eads_with_extrefs[f"{s3_bucket}/{key}"] = {
+                        "ark": ark,
+                        "s3_url": s3_url,
                         "cinco_hrefs": hrefs
                     }
-                    print(filename, len(hrefs))
+                    print(key, len(hrefs))
 
                     break
 
