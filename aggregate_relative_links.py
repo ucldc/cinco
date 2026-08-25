b2af3c4ad1214303145260a46f3270828b33c7f2
Barbara Hui <barbara.hui@ucop.edu>
2026-08-25 10:12:54 -0700

    Update aggregation script

--- aggregate_relative_links.py
+++ aggregate_relative_links.py
@@ -3,15 +3,23 @@ import json
 
 report_dir = "/Users/bhui/dev/cinco/ead_relative_links"
 aggregated = {}
-total_eads_affected = 0
+total_listed = 0
+seen = set()
+duplicates = []
 total_hrefs = 0
-report_filenames = sorted( os.listdir(report_dir))
+report_filenames = os.listdir(report_dir)
 for report_filename in report_filenames:
     with open(f"{report_dir}/{report_filename}", "r") as f:
         content = f.read()
         content = json.loads(content)
         page_ead_count = len(content)
-        total_eads_affected += page_ead_count
+        total_listed += page_ead_count
+        for s3_key in content:
+            if s3_key in seen:
+                duplicates.append(s3_key)
+                print(f"Duplicate: {report_filename}")
+            else:
+                seen.add(s3_key)
         page_href_count = 0
         for ead_filename in content:
             page_href_count += len(content[ead_filename]["cinco_hrefs"])
@@ -20,8 +28,10 @@ for report_filename in report_filenames:
 
         aggregated.update(content)
 
-print(f"Total affected eads: {total_eads_affected}")
+print(f"Total listed s3 files: {total_listed}")
+print(f"Total aggregated eads: {len(aggregated)}")
+print(f"Duplicates: {json.dumps(duplicates)}")
 print(f"Total hrefs in report: {total_hrefs}")
 
 with open("eads_with_unexpanded_otherfindaid_links.json", "w") as f:
-    f.write(json.dumps(aggregated))
+    f.write(json.dumps(dict(sorted(aggregated.items()))))
