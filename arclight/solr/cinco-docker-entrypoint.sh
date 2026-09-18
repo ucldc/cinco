53dc2bcc5718ba0a96a886578214f247235dbabd
Amy Wieliczka <amywieliczka@gmail.com>
2026-09-18 11:59:08 -0700

    [solr] move replication role config to the end

--- arclight/solr/cinco-docker-entrypoint.sh
+++ arclight/solr/cinco-docker-entrypoint.sh
@@ -6,19 +6,6 @@ if [ -f "/var/solr/data/arclight/data/index/write.lock" ]; then
     rm /var/solr/data/arclight/data/index/write.lock
 fi
 
-# Run script to configure as leader/follower for solr index replication
-if [ "$REPLICATION_ROLE" != "" ]; then
-    python3 /solr-replication-config.py
-    if [[ "$?" != 0 ]]; then
-        echo "Error running solr-replication-config.py"
-        exit 1
-    else
-        echo "Configured solr for replication role: $REPLICATION_ROLE"
-    fi
-else
-    echo "REPLICATION_ROLE env var not set; skipping configuration for solr index replication."
-fi
-
 # Run helper script to initialize an empty solr
 init-var-solr
 
@@ -33,6 +20,19 @@ else
     # copy current config into place even if core already exists
     cp -r "$config_source/." "$coredir/"
     echo "Core $CORE already exists"
+fi
+
+# Run script to configure as leader/follower for solr index replication
+if [ "$REPLICATION_ROLE" != "" ]; then
+    python3 /solr-replication-config.py
+    if [[ "$?" != 0 ]]; then
+        echo "Error running solr-replication-config.py"
+        exit 1
+    else
+        echo "Configured solr for replication role: $REPLICATION_ROLE"
+    fi
+else
+    echo "REPLICATION_ROLE env var not set; skipping configuration for solr index replication."
 fi
 
 # start solr
